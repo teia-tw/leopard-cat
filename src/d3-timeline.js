b21925659c8f44e7459f263151f54e5d8881348d
Pomin Wu <pomin5@gmail.com>
2015-07-15 17:38:38 +0800

    add timeline

---
+++ src/d3-timeline.js
@@ -0,0 +1,51 @@
+;(function () {
+  d3.layout.timeline = function () {
+    var setting = {}
+
+    function timeline (data) {
+      if (! setting.index) {
+        setting.index = setting.datetime
+      }
+      return data.sort(function (a, b) {
+        return setting.datetime(a) - setting.datetime(b)
+      }).map(function () {
+        return {
+          "x": setting.scale(setting.index.apply(null, arguments)),
+          "content": setting.content.apply(null, arguments)
+        }
+      })
+    }
+
+    timeline.nodes = timeline
+
+    ;['datetime', 'index', 'scale', 'content'].forEach(function (n) {
+      setting[n] = null
+      timeline[n] = function () {
+        if (arguments.length === 0) {
+          return setting[n]
+        }
+        setting[n] = arguments[0]
+        return timeline
+      }
+    })
+
+    setting.datetime = function (d) {
+      if (typeof d.datetime === 'string') {
+        return new Date(d.datetime)
+      } else if (typeof d.datetime === 'object') {
+        return d.datetime
+      }
+      return null
+    }
+
+    setting.content = function (d) {
+      if (d.content) {
+        return d.content
+      } else {
+        return d
+      }
+    }
+
+    return timeline
+  }
+})()
---
+++ src/timeline.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var d3 = require('d3')
+require('./d3-timeline')
+
+function timeline (selection) {
+  var width = 30
+  var height
+  var margin = { top: 0, right: 0, bottom: 0, left: 30 }
+
+  //d3.csv('/data/timeline.csv', function (err, data) {
+  d3.csv('https://docs.google.com/spreadsheets/d/1J3Sm3MURwI9ZErjcxdxNEkm9Cfactw0Na6KD65NcYcA/pub?output=csv', function (err, data) {
+    console.log(data)
+    height = data.length * 100
+
+    var svg = selection.append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+    var timeAxis = svg.append('line')
+      .attr('x1', 0)
+      .attr('y1', 0)
+      .attr('x2', 0)
+      .attr('y2', height)
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2)
+
+    var timeScale = d3.scale.linear()
+      .domain([-1, data.length])
+      .range([0, height])
+    var timeline = d3.layout.timeline()
+      .scale(timeScale)
+      .index(function (d, i) { return i })
+      .datetime(function (d) { return new Date(d['日期 ']) })
+      .content(function (d) { return '<h3>' + d['日期 '] + '</h3>' + ' [' + d['分類（以逗號分隔：開發案, 路殺, 衝突, 友善農耕,石虎研究）'].split(/,\s*/).join('][') + '] ' + '<a href="' + d['資訊連結'] + '" target="_blank">' + d['事件'] + '</a>'})
+
+    var nodes = timeline.nodes(data)
+    var node = svg.selectAll('g.event')
+      .data(nodes)
+    .enter().append('g')
+      .attr('class', 'event')
+      .attr('transform', function (d) { return 'translate(0,' + (d.x + 20) + ')' })
+
+    node.append('circle')
+      .attr('r', 5)
+      .attr('fill', 'black')
+
+    node.each(function (d) {
+      selection.append('div')
+        .attr('class', 'content')
+        .html(d.content)
+        .attr('style', 'position: absolute; left: ' + (margin.left + 30) + 'px; top: ' + d.x + 'px;')
+    })
+  })
+}
+
+d3.select('#timeline')
+  .call(timeline)
