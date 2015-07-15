9a6c1cfcec5b899643ff08f8e6e4bb7cece5f56b
Pomin Wu <pomin5@gmail.com>
2015-07-15 21:12:06 +0800

    add more info

--- src/timeline.js
+++ src/timeline.js
@@ -32,7 +32,7 @@ function timeline (selection) {
       .scale(timeScale)
       .index(function (d, i) { return i })
       .datetime(function (d) { return new Date(d['日期 ']) })
-      .content(function (d) { return '<h3>' + d['日期 '] + '</h3>' + ' [' + d['分類（以逗號分隔：開發案, 路殺, 衝突, 友善農耕,石虎研究）'].split(/,\s*/).join('][') + '] ' + '<a href="' + d['資訊連結'] + '" target="_blank">' + d['事件'] + '</a>'})
+      .content(function (d) { return '<h3>' + d['日期 '] + '</h3><p>' + ' [' + d['分類（以逗號分隔：開發案, 路殺, 衝突, 友善農耕,石虎研究）'].split(/,\s*/).join('][') + '] ' + '<a href="' + d['資訊連結'] + '" target="_blank">' + d['事件'] + '</a></p><div style="font-size: 12px">' + d['資料來源（e-info或其他媒體）'] + (d['經度（路殺或目擊事件才需登）'] ? ' (' + d['經度（路殺或目擊事件才需登）'] + ',' + d['緯度（路殺或目擊事件才需登）'] + ')' : '') + '</div>'})
 
     var nodes = timeline.nodes(data)
     var node = svg.selectAll('g.event')
