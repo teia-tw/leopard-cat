1231cce057da6a042e2379e417b0e8d9851cb759
Pomin Wu <pomin5@gmail.com>
2015-08-22 17:31:07 +0800

    publish: add polyfill for Object.assign
    generated from commit e184b602e827248e9b5288cd8b9275ebfca31d73

--- app.js
+++ app.js
@@ -21341,7 +21341,7 @@ module.exports = action
 require('debug').disable('*')
 
 var d3 = require('d3')
-var $header, $map, $timeline
+var $map, $timeline
 
 var store = require('./store')
 
@@ -21349,8 +21349,40 @@ var map = require('./map')
 var timeline = require('./timeline')
 // var tagsline = require('./tagsline')
 
+if (!Object.assign) {
+  Object.defineProperty(Object, 'assign', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function (target) {
+      'use strict'
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert first argument to object')
+      }
+
+      var to = Object(target)
+      for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i]
+        if (nextSource === undefined || nextSource === null) {
+          continue
+        }
+        nextSource = Object(nextSource)
+
+        var keysArray = Object.keys(Object(nextSource))
+        for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+          var nextKey = keysArray[nextIndex]
+          var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey)
+          if (desc !== undefined && desc.enumerable) {
+            to[nextKey] = nextSource[nextKey]
+          }
+        }
+      }
+      return to
+    }
+  })
+}
+
 function init () {
-  $header = d3.select('header')
   $map = d3.select('.map')
   $timeline = d3.select('.timeline')
   store.init()
@@ -21360,7 +21392,7 @@ function draw () {
   var width = parseInt(d3.select('body').style('width'), 10)
   $timeline.call(timeline({ width: ((width - 20) / 2) - 1, height: store.get('height') || 0, focusedEvent: store.get('focusedEvent') }))
   $map.call(map({ width: ((width - 20) / 2) - 1, date: store.get('focused') !== undefined ? store.get('focused').date : undefined }))
-  // $tagsline.call(tagsline({ width: 100, height: store.get('height') || 0 }))
+// $tagsline.call(tagsline({ width: 100, height: store.get('height') || 0 }))
 }
 
 store.on('update', draw)
